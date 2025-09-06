b57f25574b46237dd4ebafbb21757d04c752cd01
icarusaaron8 <icarusaaron8@gmail.com>
2025-09-06 22:58:49 +0700

    Add files via upload

---
+++ COMP6047001 - Algorithm and Programming - Icarus Aaron Halim.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int length = 17;
+    int width = 9;
+    int area = length * width;
+    printf("Length: %d\n", length);
+    printf("Width: %d\n", width);
+    printf("Area: %d\n", area);
+    return 0;
+}
